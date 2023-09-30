2344b56a89b9ff22380cd0135ddc58f6d9e07314
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2023-10-01 07:03:56 +0900

    init

---
+++ src/main/kotlin/com/example/springbootkotlinexposedsample/SpringbootKotlinExposedSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootkotlinexposedsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinExposedSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinExposedSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootkotlinexposedsample/SpringbootKotlinExposedSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootkotlinexposedsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinExposedSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
