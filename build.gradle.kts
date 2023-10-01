ee1c37abbd78896a7c5622d6135ddcf31b68b4a9
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2023-10-01 18:00:56 +0900

    chore: set up Exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val exposedVersion: String by project
+
 plugins {
 	id("org.springframework.boot") version "3.1.4"
 	id("io.spring.dependency-management") version "1.1.3"
@@ -22,6 +24,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+	implementation("me.paulschwarz:spring-dotenv:4.0.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/kotlin/com/example/springbootkotlinexposedsample/SpringbootKotlinExposedSampleApplication.kt
+++ src/main/kotlin/com/example/springbootkotlinexposedsample/SpringbootKotlinExposedSampleApplication.kt
@@ -1,9 +1,16 @@
 package com.example.springbootkotlinexposedsample
 
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ImportAutoConfiguration(
+	value = [ExposedAutoConfiguration::class],
+	exclude = [DataSourceTransactionManagerAutoConfiguration::class]
+)
 class SpringbootKotlinExposedSampleApplication
 
 fun main(args: Array<String>) {
